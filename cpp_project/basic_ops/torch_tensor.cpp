5da41674c7845608e10179a0667d563e293c097d
Liu Hang <liuhang@aiostchina.com>
2021-01-26 13:34:22 +0800

    veryfy torch::from_blob() func

--- cpp_project/basic_ops/torch_tensor.cpp
+++ cpp_project/basic_ops/torch_tensor.cpp
@@ -17,12 +17,33 @@
  */
 #include <stdlib.h>
 #include <torch/torch.h>
+#include <vector>
+#include <iostream>
 
 
 
-
-void main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
+  torch::Tensor x = torch::zeros({3,4});
 
-  return ;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+      x[i][j] = i*4+j;
+    }
+  }
+      
+  std::cout << "x =" << std::endl;
+  std::cout << (x) << std::endl << std::endl;
+
+  std::vector<int32_t> v = {1,2,7,8}; 
+  auto idx = torch::from_blob(v.data(), v.size(), torch::kInt32);
+  std::cout << (idx) << std::endl << std::endl;
+
+  std::cout << "<<< tensor.gather >>>" << std::endl;
+  torch::Tensor g = torch::ones({2,2}, torch::kInt64);
+  std::cout << (x.gather(-1, g)) << std::endl << std::endl;
+  // std::cout << (torch::gather(x, 0, g, false)) << std::endl << std::endl;
+  //
+
+  return 0;
 }
